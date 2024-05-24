fc64b021cbc7fa7e610707c972d0b868c1fddeaa
Basha MEGA <adambashaahmednaji@gmail.com>
2024-05-24 15:51:20 +0000

    putting it together

--- celestialScript/src/main.rs
+++ celestialScript/src/main.rs
@@ -1,3 +1,22 @@
+mod lexer;
+mod parser;
+mod interpreter;
+
+use lexer::Lexer;
+use parser::Parser;
+use interpreter::Interpreter;
+
 fn main() {
-    println!("Hello, world!");
+    let input = String::from("
+        let x = 10;
+        let y = 20;
+        print x + y;
+    ");
+
+    let lexer = Lexer::new(input);
+    let mut parser = Parser::new(lexer);
+    let statements = parser.parse();
+
+    let mut interpreter = Interpreter::new();
+    interpreter.interpret(statements);
 }
